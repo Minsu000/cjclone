4d7348d731bc656249e9896d464d322cd372c397
Minsu <kevindo00310@gmail.com>
2024-07-10 11:28:38 +0900

    swiper function added

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var swiper = new Swiper(".pics", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+}
