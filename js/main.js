326678eddb5ede097e45ed8221476b9332889972
Minsu <kevindo00310@gmail.com>
2024-07-16 09:30:52 +0900

    전면수정

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
-    $(".navi > li").mouseover(function(){
-        // console.log(this);
-        $(this).find(".submenu").stop().slideDown()
-    }).mouseout(function(){
-        $(this).find(".submenu").stop().slideUp()
-    })
+  $(".navi > li").mouseover(function(){
+      // console.log(this);
+      $(this).find(".submenu").stop().slideDown()
+  }).mouseout(function(){
+      $(this).find(".submenu").stop().slideUp()
+  })
 })
-
 
 window.onload = function(){
     var swiper = new Swiper(".pics", {
