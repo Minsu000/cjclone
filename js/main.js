8450432ec5a254b0601bfb48dcaf9511a1eba772
Minsu <kevindo00310@gmail.com>
2024-07-17 15:31:22 +0900

    슬라이드 추가

--- js/main.js
+++ js/main.js
@@ -8,12 +8,22 @@ $(document).ready(function(){
 })
 
 window.onload = function(){
-    var swiper = new Swiper(".pics", {
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
+  var swiper = new Swiper(".pics", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 }
 
 
