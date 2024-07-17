afe4b6fae1a25a6f6d958dfd0e9d695145335adc
Minsu <kevindo00310@gmail.com>
2024-07-17 16:41:51 +0900

    탑 버튼 추가

--- js/main.js
+++ js/main.js
@@ -51,7 +51,24 @@ window.onload = function(){
       controlScroll(false);
     });
 
-
+    // top버튼 스크롤 기능=======================
+    const topBtn = document.getElementById("top-btn");
+    topBtn.addEventListener("click", function (event) {
+      event.preventDefault();
+      console.log(window.scrollY);
+      if (window.scrollY == 0) {
+        window.scrollTo({
+          top: 2777,
+          behavior: "smooth",
+        });
+      } 
+      else {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      }
+    });
 
 
 
